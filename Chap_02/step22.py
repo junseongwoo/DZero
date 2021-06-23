9cd59b89d74e13fc68b64269b897bc41351c82a4
jun <wnstjd4852@gmail.com>
2021-06-24 01:17:00 +0900

    제2고지 커밋

--- Chap_02/step22.py
+++ Chap_02/step22.py
@@ -255,7 +255,52 @@ def sub(x0, x1):
 
 Variable.__sub__ = sub
 
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+Variable.__rsub__ = rsub
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0 / x1
+        return y
+    def backward(self, grad):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+Variable__truediv__ = div
+Variable__rtruediv__ = rdiv
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c 
+
+    def forward(self, x):
+        y = x ** self.c
+        return y 
+    
+    def backward(self, grad):
+        x = self.inputs[0].data
+        c = self.c 
+        gx = c * x ** (c-1) * gy
+        return gx
+
+def pow(x, c):
+    return Pow(c)(x)
+
+Variable.__pow__ = pow 
+
 if __name__ == "__main__":
     x = Variable(np.array(2.0))
-    y = -x
+    y = x ** 3
     print(y)
